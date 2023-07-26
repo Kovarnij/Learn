da82bf99d9beb7081cb3ebe7cb6a447099f797a6
Игорь <@DESKTOP-E47SAAK>
2023-07-26 20:02:29 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Learn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+            int programCount;
+            int money;
+            int time;
+            bool truth;
+
+            name = "Best";
+            age = 35;
+            programCount = 2;
+            money = 9999;
+            time = 1;
+            truth = true;
+
+            Console.WriteLine("Здравствуйте, меня зовут " + name + ".");
+            Console.WriteLine("Мне " + age + " лет.");
+            Console.WriteLine("Я хочу научиться делать игры, это моя " + programCount + " программа на языке C#.");
+            Console.WriteLine("Когда я закончу обучение я хочу зарабатывать " + money + " денег в " + time + " наносекунд.");
+            Console.WriteLine("Достигну ли я желаемого? " + truth + "!");
+
+            Console.ReadKey();
+        }
+    }
+}
