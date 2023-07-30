49374b960858f41112e1110605ffaddda9df1704
Игорь <igr22.87@gmail.com>
2023-07-30 08:12:07 +0300

    Строки

---
+++ Class3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn
+{
+    public class Class3
+    {
+        public void Less6()
+        {
+            string s1 = "Каждый ";
+            string s2 = "охотник ";
+            string s3 = "желает ";
+            string s4 = "знать, ";
+            string s5 = "где ";
+            string s6 = "сидит ";
+            string s7 = "фазан.";
+
+            Console.Write(s1);
+            Console.Write(s2);
+            Console.Write(s3);
+            Console.Write(s4);
+            Console.Write(s5);
+            Console.Write(s6);
+            Console.WriteLine(s7);
+
+            string concatString = s1+s2+s3+s4+s5+s6+s7;
+
+            Console.WriteLine(concatString);
+
+            string interpolatedString = $"{s1}{s2}{s3}{s4}{s5}{s6}{s7}";
+
+            Console.WriteLine(interpolatedString);
+        }
+    }
+}
