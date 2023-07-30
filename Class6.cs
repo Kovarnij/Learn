7ebe35c4cb92a274f6618ea4f3c82a8f0bfcb20e
Игорь <igr22.87@gmail.com>
2023-07-30 08:31:49 +0300

    logic

---
+++ Class6.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn
+{
+    public class Class6
+    {
+        public void Less9()
+        {
+            int magicNumber = 4;
+
+            Console.WriteLine("Угадай число");
+            Console.WriteLine();
+            Console.WriteLine("Загадано число от 1 до 5. Попробуйте его отгадать!");
+            Console.WriteLine();
+            Console.Write("Введите ваше число: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            //
+
+            if (input == magicNumber)
+            {
+                Console.WriteLine("Да! Это число " + magicNumber + "!");
+            }
+            else
+            {                
+                Console.WriteLine("Нет! Это не " + input + "!");
+            }
+        }
+    }
+}
