8f1aad83e6886c48ce9a904494560ef8fa0d2c26
Игорь <igr22.87@gmail.com>
2023-07-30 09:24:32 +0300

    Less11

--- Class11.cs
+++ Class11.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Learn
 {
-    internal class Class11
+    public class Class11
     {
+        public void Less11()
+        {
+            int[] numbers = new int[] { 1, 3, 5, 7, 11, 13, 17 };
+
+            Console.WriteLine("Квадраты чисел");
+            Console.WriteLine();
+            Console.Write("Дан ряд чисел:");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($" {numbers[i]} ");
+            }
+
+            Console.WriteLine();
+            Console.Write("Квадраты этих чисел:");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($" {numbers[i] * numbers[i]} ");
+            }
+        }
     }
 }
