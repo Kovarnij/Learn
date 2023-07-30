e4a60b16f5358de4bdbb67dcaddda40324d35f51
Игорь <igr22.87@gmail.com>
2023-07-30 08:23:43 +0300

    ReadLine

---
+++ Class5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn
+{
+    public class Class5
+    {
+        public void Less8()
+        {
+            Console.Write("Введите своё имя: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.Write("Введите свой возраст: ");
+            string age = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.Write($"Благодарим, {name} ({age}), за введенную информацию! Очень важный спам будет отправляться вам на почту!");
+        }
+    }
+}
