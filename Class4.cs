dc92a25d2817d264c08389024ffbff7d51c4a502
Игорь <igr22.87@gmail.com>
2023-07-30 08:17:54 +0300

    Convert

---
+++ Class4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn
+{
+    public class Class4
+    {
+        public void Less7()
+        {
+            string stringNumber = "256";
+
+            int numberFromString = Convert.ToInt32(stringNumber);
+
+            int square = numberFromString * numberFromString;
+
+            Console.WriteLine("Квадрат введенного числа равен " + square);
+
+        }
+    }
+}
